aa4a6438663a62b80c9d9412df0bd235a0ffaa40
pritam992 <pritambehura1992@gmail.com>
2021-08-24 23:32:55 +0530

    updated template

--- Terraform/Solution1/main.tf
+++ Terraform/Solution1/main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "web" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
+  key_name      = "amlanjp"
+  instance_count = 1
+  name          = "testing-server"
 
   tags = {
     name = "${var.name}"
